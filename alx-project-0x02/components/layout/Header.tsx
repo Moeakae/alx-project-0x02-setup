73a7e717a8a893e97f6ad540931ca5a374aa124d
Moeakae <ntsoakilekhoakhoa@gmail.com>
2025-07-27 18:22:31 +0200

    updated

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,23 +1,23 @@
 // components/layout/Header.tsx
-
-
-import React from 'react';
 import Link from 'next/link';
 
-
-
 const Header = () => {
-  return ( 
-    <header className="flex gap-4 p-4 bg-gray-100 shadow-md">
-      <Link href="/home" className="text-blue-600 hover:underline">Home</Link>
-      <Link href="/about" className="text-blue-600 hover:underline">About</Link>
+  return (
+    <header className="bg-blue-600 p-4 text-white">
+      <nav className="flex space-x-4">
+        <Link href="/home">
+          <span className="hover:underline cursor-pointer">Home</span>
+        </Link>
+        <Link href="/about">
+          <span className="hover:underline cursor-pointer">About</span>
+        </Link>
+        <Link href="/posts">
+          <span className="hover:underline cursor-pointer">Posts</span>
+        </Link>
+      </nav>
     </header>
-    
-
-
-
   );
 };
 
-
 export default Header;
+
