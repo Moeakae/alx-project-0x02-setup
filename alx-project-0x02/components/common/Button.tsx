5a2aa829d3af14b381348aafb698dd0e31abfad4
Moeakae <ntsoakilekhoakhoa@gmail.com>
2025-07-27 18:36:45 +0200

    updated

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -1,27 +1,27 @@
-// components/Button.tsx
-import React from 'react';
- import { ButtonProps } from '@/interfaces';
+// components/common/Button.tsx
+import { ButtonProps } from '@/interfaces';
 
-type ButtonProps = {
-  title: string;
-  size?: 'small' | 'medium' | 'large';
-  shape?: 'rounded-sm' | 'rounded-md' | 'rounded-full';
-};
 
-const sizeClasses = {
-  small: 'text-sm px-3 py-1',
-  medium: 'text-base px-4 py-2',
-  large: 'text-lg px-5 py-3',
-};
+const Button = ({ label, size, shape }: ButtonProps) => {
+  const sizeClasses = {
+    small: 'px-3 py-1 text-sm',
+    medium: 'px-4 py-2 text-base',
+    large: 'px-6 py-3 text-lg',
+  };
 
-const Button: React.FC<ButtonProps> = ({
-  title,
-  size = 'medium',
-  shape = 'rounded-md',
-}) => {
-  const finalClass = `bg-blue-600 text-white font-semibold ${sizeClasses[size]} ${shape}`;
-
-  return <button className={finalClass}>{title}</button>;
-};
+  return (
+    <button
+      className={
+        'bg-blue-600 text-white font-semibold';
+        sizeClasses[size];
+        shape
+      }
+    >
+      {label}
+    </button>
+  )
+}
 
 export default Button;
+
+
