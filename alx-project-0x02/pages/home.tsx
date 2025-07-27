33d7e595564a15ba2bdfd46803fef47a227b9e08
Moeakae <ntsoakilekhoakhoa@gmail.com>
2025-07-27 15:37:30 +0200

    updated

---
+++ alx-project-0x02/pages/home.tsx
@@ -0,0 +1,12 @@
+// pages/home.tsx
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold">Welcome to the Home Page</h1>
+    </div>
+  );
+};
+
+export default Home;
