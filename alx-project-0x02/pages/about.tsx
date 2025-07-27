2ff92476bc1722b723cf9638dcf8a51bc93f3315
Moeakae <ntsoakilekhoakhoa@gmail.com>
2025-07-27 17:29:13 +0200

    updated

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <div className="p-8">
       <h1 className="text-3xl font-bold">About Us</h1>
-      import Header from '@/components/layout/Header';
+  
     <>
       <Header />
       <main className="p-4">
